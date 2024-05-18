39b49970df02cf7760fc43fde363242d52747a24
tk1351 <takakikitsui0111@gmail.com>
2024-05-19 08:45:25 +0900

    fix: remove index.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./reset.css";
-import "./index.css";
 
 // vite のテンプレートが non-null assertion のため
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
