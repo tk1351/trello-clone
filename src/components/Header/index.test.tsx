a60c414b0efb7409a03bf1c3e3944fde7f1cecaf
tk1351 <takakikitsui0111@gmail.com>
2024-05-19 20:37:42 +0900

    chore: format

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,22 +1,22 @@
-import '@testing-library/jest-dom'
-import {render, screen} from "@testing-library/react";
-import {Header} from "./index.tsx";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { Header } from "./index.tsx";
 
-describe('Header Component', () => {
-    describe('a11y', () => {
-        it('[role="banner"]が存在すること', () => {
-            render(<Header />)
-            expect(screen.getByRole('banner')).toBeInTheDocument()
-        })
-        it('[role="heading"]が存在すること', () => {
-            render(<Header />)
-            expect(screen.getByRole('heading')).toBeInTheDocument()
-        })
-    })
-    describe('render', () => {
-        it('"Trello Clone" と表示されていること', () => {
-            render(<Header />)
-            expect(screen.getByText('Trello Clone')).toBeInTheDocument()
-        })
-    })
-})
+describe("Header Component", () => {
+	describe("a11y", () => {
+		it('[role="banner"]が存在すること', () => {
+			render(<Header />);
+			expect(screen.getByRole("banner")).toBeInTheDocument();
+		});
+		it('[role="heading"]が存在すること', () => {
+			render(<Header />);
+			expect(screen.getByRole("heading")).toBeInTheDocument();
+		});
+	});
+	describe("render", () => {
+		it('"Trello Clone" と表示されていること', () => {
+			render(<Header />);
+			expect(screen.getByText("Trello Clone")).toBeInTheDocument();
+		});
+	});
+});
