81eb41efc63154671100f3d60012860316c00e94
tk1351 <takakikitsui0111@gmail.com>
2024-05-19 09:32:11 +0900

    feat: add background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Header } from "./components/Header";
+import styles from './App.module.css'
 
 function App() {
 	return (
-		<>
+		<div className={styles.container}>
 			<Header />
-		</>
+		</div>
 	);
 }
 
